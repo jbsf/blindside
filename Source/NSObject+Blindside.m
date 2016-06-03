388e4893dc6e0348282235d64dbfc166dd8858b5
Patrick Barry <patrick@lumoslabs.com>
2016-06-03 15:13:22 -0700

    adds protocols to inject dependencies object with reduced boilerplate

--- Source/NSObject+Blindside.m
+++ Source/NSObject+Blindside.m
@@ -2,6 +2,7 @@
 #import "BSInitializer.h"
 #import "BSInjector.h"
 #import "BSPropertySet.h"
+#import <objc/runtime.h>
 
 static NSString *const BSMissingInitializerSpecificationException = @"BSMissingInitializerSpecificationException";
 
@@ -30,15 +31,74 @@ static NSString *const BSMissingInitializerSpecificationException = @"BSMissingI
 }
 
 + (BSInitializer *)bsInitializer {
+    // NOTE: this 'if' clause is a Lumos Lab addition
+    if ([self conformsToProtocol:@protocol(BSDependencyInjectable)]) {
+        return [BSInitializer initializerWithClass:self selector:@selector(initWithDependencies:) argumentKeysArray:@[[self dependencyProvidingClass]]];
+    }
+    
     return nil;
 }
 
 + (BSPropertySet *)bsProperties {
+    // NOTE: this 'if' clause is a Lumos Labs addition
+    if ([self conformsToProtocol:@protocol(BSDependencyProviding)]) {
+        NSMutableDictionary *propertiesDict = [NSMutableDictionary dictionary];
+        
+        unsigned int propertyCount;
+        objc_property_t *properties = class_copyPropertyList(self, &propertyCount);
+        for (int propertyIndex = 0; propertyIndex < propertyCount; ++propertyIndex) {
+            NSString *propertyName;
+            NSString *propertyType;
+            
+            unsigned int attributeCount;
+            objc_property_attribute_t *attributes = property_copyAttributeList(properties[propertyIndex], &attributeCount);
+            for (int attributeIndex = 0; attributeIndex < attributeCount; ++attributeIndex) {
+                objc_property_attribute_t attribute = attributes[attributeIndex];
+                if (*attribute.name == 'T') {
+                    propertyType = [@(attribute.value) stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"@\""]];
+                }
+                else if (*attribute.name == 'V') {
+                    propertyName = [@(attribute.value) substringFromIndex:1]; // cut off leading "_"
+                }
+            }
+            free(attributes);
+            
+            if (propertyName && propertyType) {
+                propertiesDict[propertyName] = propertyType;
+            }
+        }
+        free(properties);
+        
+        NSArray *propertyNames = [propertiesDict allKeys];
+        BSPropertySet *propertySet = [BSPropertySet propertySetWithClass:self propertyNamesArray:propertyNames];
+        for (NSString *name in propertyNames) {
+            NSString *type = propertiesDict[name];
+            if ([type UTF8String][0] == '<') {
+                type = [type stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+                [propertySet bindProperty:name toKey:NSProtocolFromString(type)];
+            } else {
+                [propertySet bindProperty:name toKey:NSClassFromString(type)];
+            }
+        }
+        
+        return propertySet;
+    }
+    
     return nil;
 }
 
 - (void)bsAwakeFromPropertyInjection {
     
+}
+
+#pragma mark - Lumos Labs additions
+
+#pragma mark - BSDependencyInjectable
+
++ (Class)dependencyProvidingClass
+{
+    NSString *className = NSStringFromClass(self);
+    return NSClassFromString([className stringByAppendingString:@"_Dependencies"]);
 }
 
 @end
