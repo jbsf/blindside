31eb2f80f90cced33a80b99acb298b90e3503045
Andrew Kitchen <akitchen@gmail.com>
2017-10-06 10:16:44 -0700

    Updates to SWIFT_VERSION to 4.0
    Includes other Xcode housekeeping

--- Specs/SwiftFixtures.swift
+++ Specs/SwiftFixtures.swift
@@ -3,26 +3,26 @@ import Blindside
 
 class SwiftState : NSObject {}
 class SwiftCity : NSObject {
-    var population = 0
+    @objc var population = 0
 }
 
 class SwiftGarage : NSObject {
-    var empty = true
+    @objc var empty = true
 }
 
 class SwiftDriveway : NSObject {}
 
 class SwiftAddress : NSObject {
-    var street: String
-    var city: SwiftCity
-    var state: SwiftState
-    var zip: String
+    @objc var street: String
+    @objc var city: SwiftCity
+    @objc var state: SwiftState
+    @objc var zip: String
 
     static override func bsInitializer() -> BSInitializer {
-        return BSInitializer(withClass: self, selector: "initWithStreet:city:state:zip:", argumentKeysArray: ["street", "city", "state", "zip"])
+        return BSInitializer(with: self, selector: #selector(SwiftAddress.init(street:city:state:zip:)), argumentKeysArray: ["street", "city", "state", "zip"])
     }
 
-    init(street: String, city: SwiftCity, state: SwiftState, zip: String) {
+    @objc init(street: String, city: SwiftCity, state: SwiftState, zip: String) {
         self.street = street
         self.city = city
         self.state = state
@@ -31,17 +31,17 @@ class SwiftAddress : NSObject {
 }
 
 class SwiftHouse : NSObject {
-    var address: SwiftAddress
-    var garage: SwiftGarage!
-    var driveway: SwiftDriveway!
-    weak var injector: BSInjector!
+    @objc var address: SwiftAddress
+    @objc var garage: SwiftGarage!
+    @objc var driveway: SwiftDriveway!
+    @objc weak var injector: BSInjector!
 
     static override func bsInitializer() -> BSInitializer {
-        return BSInitializer(withClass: self, selector: "initWithAddress:", argumentKeysArray: [SwiftAddress.self])
+        return BSInitializer(with: self, selector: #selector(SwiftHouse.init(address:)), argumentKeysArray: [SwiftAddress.self])
     }
 
     class override func bsProperties() -> BSPropertySet {
-        let propertySet = BSPropertySet(withClass: self, propertyNamesArray: ["garage", "driveway"])
+        let propertySet = BSPropertySet(with: self, propertyNamesArray: ["garage", "driveway"])
         propertySet.bindProperty("driveway", toKey: "theDriveway")
         return propertySet
     }
@@ -50,7 +50,7 @@ class SwiftHouse : NSObject {
         garage!.empty = true
     }
 
-    init(address: SwiftAddress) {
+    @objc init(address: SwiftAddress) {
         self.address = address
     }
 }
@@ -58,24 +58,24 @@ class SwiftHouse : NSObject {
 class SwiftTennisCourt : NSObject {}
 
 class SwiftMansion : SwiftHouse {
-    var tennisCourt: SwiftTennisCourt!
+    @objc var tennisCourt: SwiftTennisCourt!
 
     class override func bsProperties() -> BSPropertySet {
-        let propertySet = BSPropertySet(withClass: self, propertyNamesArray: ["tennisCourt"])
+        let propertySet = BSPropertySet(with: self, propertyNamesArray: ["tennisCourt"])
         propertySet.bindProperty("driveway", toKey: "10 car driveway")
         return propertySet
     }
 }
 
 class SwiftClassWithFactoryMethod : NSObject {
-    var foo: String
-    var bar: String
+    @objc var foo: String
+    @objc var bar: String
 
-    static override func bsCreateWithArgs(args: [AnyObject], injector: BSInjector) -> AnyObject {
+    @objc override static func bsCreate(withArgs args: [Any], injector: BSInjector) -> Any {
         return SwiftClassWithFactoryMethod(foo: args[0] as! String, bar: injector.getInstance("bar") as! String)
     }
 
-    init(foo: String, bar: String) {
+    @objc init(foo: String, bar: String) {
         self.foo = foo
         self.bar = bar
     }
